d55f11884ac40e290df14b7e15dc1bd85a35c785
arch1e <marchbold91@gmail.com>
2015-08-04 15:52:00 -0400

    Pages inherit from the abstract page class now. Map included for xpaths and titles in the homePage class which passes info to pages parent. Thinking of removing validations before each step.

--- src/pages/HomePage.java
+++ src/pages/HomePage.java
@@ -1,35 +1,27 @@
 package pages;
 
-import org.openqa.selenium.By;
+import java.util.HashMap;
+
 import org.openqa.selenium.WebDriver;
 
-public class HomePage {
-	//testing git commit on laptop
-	private static WebDriver driver;
-	
+public class HomePage extends Page{
+
 	public HomePage(WebDriver driver){
-		HomePage.driver = driver;
-	}
-	
-	public void goTo() throws Exception{
-		driver.get("http://www.mta.info");
-		if (!driver.getTitle().equals("MTA | Subway, Bus, Long Island Rail Road, Metro-North"))
-			throw new Exception("title of homepage not found");
+		String URL = "http://www.mta.info";
+		
+		HashMap<String, String> homePageMap = new HashMap<String, String>();
+		homePageMap.put("title", "MTA | Subway, Bus, Long Island Rail Road, Metro-North");
+		homePageMap.put("neighborhoods", "mta.info | MTA Neighborhood Maps");
+		homePageMap.put("subway map", "mta.info | MTA Subway Map");
+		
+		Page.setPage(URL, driver, homePageMap);
 	}
 	
 	public void clickScheduleButton() throws Exception{
-		if (!driver.getTitle().equals("MTA | Subway, Bus, Long Island Rail Road, Metro-North"))
-			throw new Exception("homepage not found");
-		driver.findElement(By.xpath(".//*[@id='menu-415-1']/a")).click();
-		if (!driver.getTitle().equals("mta.info | MTA Neighborhood Maps"))
-			throw new Exception("fuck off");
+		clickXPath("title", "neighborhoods", ".//*[@id='menu-415-1']/a");
 	}
 	
 	public void clickSubwayMap() throws Exception{
-		if (!driver.getTitle().equals("mta.info | MTA Neighborhood Maps"))
-			throw new Exception("schedule page not found");
-		driver.findElement(By.linkText("Subway System")).click();
-		if (!driver.getTitle().equals("mta.info | MTA Subway Map"))
-			throw new Exception("subway map page not found");
+		clickLinkText("neighborhoods", "subway map", "Subway System");
 	}
 }
--- src/pages/Page.java
+++ src/pages/Page.java
@@ -5,27 +5,53 @@ import java.util.HashMap;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class Page {
+public abstract class Page {
 	
+	public static String URL;
 	public static WebDriver driver;
+	public static HashMap<String, String> xpathMap;
 	
-	public static void setDriver(WebDriver driver){
+	public static void setPage(String URL, WebDriver driver, HashMap<String, String> xpathMap){
+		Page.URL = URL; 
 		Page.driver = driver;
+		Page.xpathMap = xpathMap;
+	}
+
+	public static void verifyPage(String key) throws Exception{
+		if (!driver.getTitle().equals(xpathMap.get(key)))
+			throw new Exception(key + " not found");
 	}
 	
-	public static void verifyPage(String pageInfo[]) throws Exception{
-		if (!driver.getTitle().equals(pageInfo[1]))
-			throw new Exception(pageInfo[0] + " not found");
-	}
-	
-	public void clickXPath(String[][] pageInfo, String xpath){
-		/*clicks inputed XPath and verifies that starting and ending pages are reached
-		 *two dimensional is for the value of the page verifications and the name of the
-		 *page if there is an error*/
+	public void goTo(){
 		try{
-			verifyPage(pageInfo[0]);
-			driver.findElement(By.xpath(xpath)).click();
-			verifyPage(pageInfo[1]);
+			driver.get(URL);
+			if (!driver.getTitle().equals(xpathMap.get("title")))
+				throw new Exception(URL + " not found");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void clickXPath(String key1, String key2, String xpath){
+		/* clicks inputed XPath and verifies that starting and ending pages are reached
+		 * two dimensional is for the value of the page verifications and the name of the
+		 * page if there is an error */
+		try{
+			verifyPage(key1);
+			driver.findElement(By.xpath(xpath)).click();		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void clickLinkText(String key1, String key2, String linkText){
+		/* validates on that is on proper page, clicks link text, validates
+		 * next page has been reach */
+		try{
+			verifyPage(key1);
+			driver.findElement(By.linkText(linkText)).click();
+			verifyPage(key2);
 		}
 		catch(Exception e){
 			e.printStackTrace();
